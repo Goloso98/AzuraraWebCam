748bdda567fb185b4ea80f3d1b2565cdd5a7da49
Gabriel <gooloso98@gmail.com>
2022-07-19 02:44:28 +0100

    Create proxy.py

---
+++ proxy.py
@@ -0,0 +1,61 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from requests import get
+import re
+
+def baseURL():
+  url = get_playlist_url()
+  endurl = url.rindex('/') + 1
+  return url[:endurl]
+
+def get_playlist_url():
+  res = get('https://surftotal.com/camaras-report/grande-porto-douro-litoral/azurara')
+  body = res.text
+  match = re.search(r"""<source src="(.*)" type="application\/x-mpegURL">""", body)
+  playlisturl = match.group(1)
+  return playlisturl
+
+def get_playlist():
+  url = get_playlist_url()
+  return get(url).content
+
+def get_chunklist():
+  baseurl = baseURL()
+  playlist = get_playlist().decode('utf-8')
+  chunklisturl = playlist.split('\n')[3]
+  return get(baseurl + chunklisturl).content
+
+def get_media(path):
+  baseurl = baseURL()
+  url = baseurl[:-1] + path
+  media = get(url).content
+  return media
+  
+
+class MyHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    if self.path.startswith('/playlist'):
+      self.send_response(200)
+      self.end_headers()
+      self.wfile.write(get_playlist())
+      return
+    if self.path.startswith('/chunklist'):
+      self.send_response(200)
+      self.end_headers()
+      self.wfile.write(get_chunklist())
+      return
+    if self.path.startswith('/media'):
+      self.send_response(200)
+      self.end_headers()
+      self.wfile.write(get_media(self.path))
+      return
+    print('server', self.server)
+    print('requestline', self.requestline)
+    print(self.server.server_name)
+    print(self.server.server_port)
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(str(self.path + '\nroot\n').encode())
+
+
+httpd = HTTPServer(('localhost', 3000), MyHandler)
+httpd.serve_forever()
